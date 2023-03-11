afc0d2610173b792d32cf6916da7cabb8e649cef
Michał Masiulanis <m.masiulanis@gmail.com>
2023-03-11 23:22:14 +0100

    Profile without css

---
+++ src/components/Profile.jsx
@@ -0,0 +1,40 @@
+import PropTypes from 'prop-types';
+import './Profile.css';
+
+const Profile = ({ username, tag, location, avatar, stats }) => {
+  return (
+    <div class="profile">
+      <div class="description">
+        <img src={avatar} alt="User avatar" class="avatar" width="200px" />
+        <p class="name">{username}</p>
+        <p class="tag">{tag}</p>
+        <p class="location">{location}</p>
+      </div>
+
+      <ul class="stats">
+        <li>
+          <span class="label">Followers</span>
+          <span class="quantity">{stats.followers}</span>
+        </li>
+        <li>
+          <span class="label">Views</span>
+          <span class="quantity">{stats.views}</span>
+        </li>
+        <li>
+          <span class="label">Likes</span>
+          <span class="quantity">{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+Profile.prototype = {
+  username: PropTypes.string,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  avatar: PropTypes.string,
+  stats: PropTypes.number,
+};
+
+export default Profile;
